c15fb7ebc40981ea42fa5e74b648209598328fc9
Philip Durbin <philip_durbin@harvard.edu>
2016-06-16 14:29:00 -0400

    add sr.py (script request), cleanup of ur.py

--- api/dcm.py
+++ api/dcm.py
@@ -2,7 +2,7 @@
 # tested with:
 # - Flask-0.11-py2.py3-none-any.whl on Python 2.7 (OS X 10.10)
 # - python-flask-0.9-7.el6.noarch on Python 2.6 (CentOS 6)
-from flask import Flask
+from flask import Flask, Response
 import json
 app = Flask(__name__)
 
@@ -11,12 +11,26 @@ def hello_world():
         return 'Hello, World!'
 
 @app.route('/ur.py', methods=['POST'])
-def login():
-    resp = {}
+def upload_request():
+    data = {}
+    data['status'] = 'OK'
+    resp = Response(response=json.dumps(data),
+        # FIXME: Should this be 201 instead?
+        status=200, \
+        mimetype="application/json")
+    return(resp)
+
+@app.route('/sr.py/<int:dataset_id>', methods=['GET'])
+def script_request(dataset_id):
+    data = {}
+    data['datasetId'] = dataset_id
     with open('rsync.sh', 'r') as myfile:
             data=myfile.read()
-    resp['script'] = data
-    return json.dumps(resp)
+    data['script'] = data
+    resp = Response(response=json.dumps(data),
+        status=200, \
+        mimetype="application/json")
+    return(resp)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
