23797e564459f81f167cf0a57ee8078aaf919870
Pete Meyer <pameyer@crystal.harvard.edu>
2016-08-05 14:30:51 -0400

    test script; lighttpd config

---
+++ ansible/roles/dcm/files/root/scripts/dcm-test01.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# test script for DCM
+
+# hardcoding localhost, because that's what it's testing
+
+if [ "$1" == "" ]; then
+	echo "format is: dcm-test01.sh [test dataset id]"
+	exit
+fi
+
+curl -H "Content-Type: application/json" -X POST -d '{"datasetId":"${1}", "userId":"testuser-t03"}' http://127.0.0.1/ur.py
+
