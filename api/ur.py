e89553cf70a0ab826f9de21db895814a7da6cef9
Pete Meyer <pameyer@crystal.harvard.edu>
2016-11-17 11:55:56 -0500

    first pass at cron -> rq

--- api/ur.py
+++ api/ur.py
@@ -5,17 +5,29 @@ upload request handler
 
 '''
 
-import sys
-import os
 DATADIR='/deposit/requests/'
+
+import sys
+sys.path.append('../gen')
+import os
+from usg import proc
+from rq import Queue
+from redis import Redis
 
 #at least with current lighttpd cgi config, PID is unique to each call
 pid = os.getpid() 
 
+# dump to unique file
 fname = '%s/%d.json' % (DATADIR, pid)
 opf = open( fname, 'w' )
 opf.write( sys.stdin.read() )
 opf.close()
+
+# put it in the queue
+conn = Redis()
+queue = Queue('normal', connection = conn )
+job = queue.enqueue( proc, fname )
+# may or may not be useful to return job identifier to caller
 
 # this sets reponse headers to 200, aparently
 print('Content-Type: text/plain\n\n')
