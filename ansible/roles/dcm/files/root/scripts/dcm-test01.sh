7a9a913186d2de6de7aba8d0743fc6b871b54c4f
Pete Meyer <pameyer@crystal.harvard.edu>
2017-04-28 12:45:22 -0400

    argument handling in test script

--- ansible/roles/dcm/files/root/scripts/dcm-test01.sh
+++ ansible/roles/dcm/files/root/scripts/dcm-test01.sh
@@ -4,11 +4,20 @@
 
 # hardcoding localhost, because that's what it's testing
 
-if [ "$1" == "" ]; then
-	echo "format is: dcm-test01.sh [test dataset id]"
-	exit
+if [ -z "$1" ]; then
+	echo "format is: dcm-test01.sh [test dataset id] [test user id]"
+	exit 1
+else
+	dset=$1
+fi
+
+if [ -z $"$2" ]; then 
+	echo "format is: dcm-test01.sh [test dataset id] [test user id]"
+	exit 1
+else
+	user=$2
 fi
 
 
-curl -H "Content-Type: application/json" -X POST -d "{\"datasetId\":\"${1}\", \"userId\":\"testuser-t03\",\"datasetIdentifier\":\"${1}\"}" http://127.0.0.1/ur.py
+curl -H "Content-Type: application/json" -X POST -d "{\"datasetId\":\"${$dset}\", \"userId\":\"${user}\",\"datasetIdentifier\":\"${1}\"}" http://127.0.0.1/ur.py
 
