efecac28ed940548d469c1c2c9b7730916360b96
Pete Meyer <pameyer@crystal.harvard.edu>
2017-05-22 11:00:11 -0400

    bugfix for issue 20; plus a little cleanup/logging

--- api/sr.py
+++ api/sr.py
@@ -7,6 +7,7 @@ API endpoint for script requests
 import cgi
 import json
 import os.path
+import sys
 
 #TODO - read from environmental variable
 UPLOAD_ROOT='/deposit/'
@@ -14,21 +15,31 @@ UPLOAD_ROOT='/deposit/'
 def proc():
     form = cgi.FieldStorage()
     try:
-        uid = form['datasetIdentifier'].value
+        # retrieve upload ID from form data
+        ulid = form['datasetIdentifier'].value
     except KeyError:
+        # meaningless to ask for a transfer script if request doesn't specify which one
         print('Status:400\n\n')
+        sys.stderr.write('no datasetIdentifierField in FORM data\n')
         return
-    fn = '/%s/gen/upload-%s.bash' % (UPLOAD_ROOT, uid)
+    fn = '/%s/gen/upload-%s.bash' % (UPLOAD_ROOT, ulid)
     if not os.path.exists( fn ):
+        # transfer script not generated; return 404 according to API docs.
+        # not logging errors, because it's not an error condition
         print('Status:404\n\n')
         return
+    # read transfer script for handoff
     with open(fn, 'r') as inp:
         dat = inp.read()
-    with open( os.path.join( UPLOAD_ROOT, 'processed', '%s.json' % uid ) ) as inp:
+    # read request file (transfer "metadata") 
+    with open( os.path.join( UPLOAD_ROOT, 'processed', '%s.json' % ulid ) ) as inp:
         req = json.load( inp )
-    if req['datasetIdentifier'] != uid:
+
+    # "validate" request file
+    if int(req['datasetIdentifier']) != (ulid):
         # should never happen - defensive programming, or paranoia depending on perspective
         print('Status:500\n\n')
+        sys.stderr.write('datasetIdentifier in request file does not match base filename\n')
         return
     req['script'] = dat
     print('Content-Type: application/json\n\n%s' % json.dumps( req ) )
