246a2bebff483be3bf41558772ba386741f7fe9c
Pete Meyer <pameyer@crystal.harvard.edu>
2017-08-03 11:47:00 -0400

    test scripts for DCM -> DV APIs (validation passed, validation failed)

---
+++ ansible/roles/dcm/files/root/scripts/send_fail.bash
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# stub for testing the DCM -> DV "checksum validation failed" API
+
+dset=EUGEQF
+sz=131111836
+tmpfile=/tmp/dcm_fail-$$.json
+
+echo "{\"status\":\"validation failed\",\"uploadFolder\":\"$dset\",\"totalSize\":$sz}" > $tmpfile
+
+cat $tmpfile | jq .
+DOI_SHOULDER="10.5072/FK2"
+
+curl -H "X-Dataverse-key: ${DVAPIKEY}" -H "Content-type: application/json" -X POST --upload-file  $tmpfile "${DVHOST}/api/datasets/:persistentId/dataCaptureModule/checksumValidation?persistentId=doi:${DOI_SHOULDER}/${dset}"
+
+
