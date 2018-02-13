c6f4df262e9d1be7a98d001cffb899babd3127df
Pete Meyer <pameyer@crystal.harvard.edu>
2018-02-13 18:13:18 -0500

    initial rq dependency source -> rpm

---
+++ rpm/dep/rq/build.bash
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+rq_version=0.8.1
+# TODO - dependency check (python setuptools, yum)
+if [ ! -e ../src/rq-${rq_version}.tar.gz ]; then
+	wget https://github.com/rq/rq/archive/v${rq_version}.tar.gz -O ../src/rq-${rq_version}.tar.gz
+fi
+tar zxf ../src/rq/rq-${rq_version}.tar.gz
+cd rq-${rq_version}
+python setup.py bdist --format=rpm
+cp dist/rq-${rq_version}-1.noarch.rpm ../../dist/
+
