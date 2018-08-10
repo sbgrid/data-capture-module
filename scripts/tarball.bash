33598b711107733b16cd051d903ec9f26c8350b5
Pete Meyer <pameyer@crystal.harvard.edu>
2018-08-10 14:54:29 -0400

    update build stuff for shared lib directory

--- scripts/tarball.bash
+++ scripts/tarball.bash
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
 # create tarball for building staging/prod rpm
-
-v=0.1
+if [ -z ${DCM_VERSION} ]; then
+	v=0.1
+else
+	v=${DCM_VERSION}
+fi
 
 if [ ! -d dist/ ]; then
 	mkdir dist
 fi
 
-tar zcf dist/dcm-${v}.tar.gz requirements.txt scn/*sh gen/*py gen/*m4 api/sr.py api/ur.py doc/config/* requirements.txt
+tar zcf dist/dcm-${v}.tar.gz requirements.txt scn/*sh gen/*py gen/*m4 api/sr.py api/ur.py lib/*py doc/config/* requirements.txt
